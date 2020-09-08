b2154d6f65a63fff3ecad5ef1229871eb397e2ea
rtomoham <robert@tomohamat.com>
2020-09-08 09:51:53 +0200

    Added output messages

--- Somtoday2GSuite.php
+++ Somtoday2GSuite.php
@@ -57,31 +57,48 @@ class Somtoday2GSuite {
   }
 
   function refresh() {
+    printMessage('START refresh');
     $response = $this->somtodayWebBroker->getAugustinianumElo();
     if ($this->htmlParser->isUpdating($response)) {
       // The site is being updated. So we cannot do anything.
+      printMessage('somtoday is updating');
       error_log('STOP: somtoday is in maintenance mode.');
     } elseif ($this->htmlParser->isError($response)) {
       // We received an error message. So we cannot do anything.
+      printMessage('somtoday threw an error');
       error_log('STOP: somtoday threw an error.');
     } else {
       if ($this->htmlParser->findSignInForm($response)) {    
+        printMessage('Sending username');
         $action = $this->htmlParser->getAction($response);
         $action = substr($action, 2);
         $auth = substr($action, strpos($action, 'auth='));
         $response = $this->somtodayWebBroker->setUsername($action, $auth, $this->somtodayUser);
       }
       if ($this->htmlParser->findPasswordForm($response)) {
+        printMessage('Sending password');
         $action = $this->htmlParser->getAction($response);
         $action = substr($action, 2);
         $response = $this->somtodayWebBroker->setPassword($action, $this->somtodayUser);
-        $this->somtodayWebBroker->redirectToAug();
       }
+      if ($this->htmlParser->findContinue($response)) {
+        printMessage('Redirecting to school site (click continue)');
+        $action = $this->htmlParser->getAction($response);
+        $action = substr($action, 2);
+        $response = $this->somtodayWebBroker->redirectToAug();
+      }
+
+      printMessage('Get student home page');
       $this->somtodayWebBroker->getStudent($this->somtodayUser->getStudentIndex());
+
+      printMessage('Get roster');
       $classes = $this->somtodayWebBroker->getRoster();
       
       if (0 < count($classes)) {
+        printMessage('Received ' . count($classes) . ' classes - sending to GSuite');
         $this->googleApiBroker->refreshCalendar($classes);
+      } else {
+        printMessage('Did not receive any classes - something probably went wrong');
       }
 
     }   
