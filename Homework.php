25a2488fb43b94734e500052dc80b92daab3573b
rtomoham <rtomoham@cisco.com>
2020-09-08 10:26:31 +0200

    Add files via upload

--- Homework.php
+++ Homework.php
@@ -1,19 +1,30 @@
 <?php
 class Homework {
-  
+
+  private $id;
+  private $serverCall;
   private $link;
   private $task;
   private $done = false;
-  
-  function __construct($task, $link) {
+
+  function __construct($id, $task, $link) {
+    $this->id = $id;
     $this->task = $task;
     $this->link = $link;
   }
-  
+
+  function getId() {
+    return $this->id;
+  }
+
   function getLink() {
     return $this->link;
   }
-  
+
+  function getServerCall() {
+    return $this->serverCall;
+  }
+
   function getTask() {
     return $this->task;
   }
@@ -21,10 +32,14 @@ class Homework {
   function isDone() {
     return $this->done;
   }
-  
+
   function setDone($done = false) {
     $this->done = $done;
   }
-  
+
+  function setServerCall($serverCall) {
+    $this->serverCall = $serverCall;
+  }
+
 }
 ?>
