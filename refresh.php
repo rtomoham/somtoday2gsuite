e87b219a109c2ae9b29d65102b12f8ca68ccda41
rtomoham <robert@tomohamat.com>
2020-09-09 08:54:32 +0200

    Added TZ and test command line argument

--- refresh.php
+++ refresh.php
@@ -1,6 +1,17 @@
 <?php
-include('Somtoday2GSuite.php');
+require_once('Somtoday2GSuite.php');
 
-$somtoday2gsuite = new Somtoday2GSuite();
+if (!date_default_timezone_set(getenv('TZ'))) {
+ date_default_timezone_set('Europe/Amsterdam');
+};
+
+if ($argc > 1) {
+  if ('test' == $argv[1]) {
+    echo("TEST: 'test' argument provided, so running test.\n\n");
+    $somtoday2gsuite = new Somtoday2GSuite(true);
+  } else {
+    $somtoday2gsuite = new Somtoday2GSuite();
+  }
+}
 $somtoday2gsuite->refresh();
- ?>
+?>
